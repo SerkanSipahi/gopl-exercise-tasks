5f62b523383ccb85ecc5b74af5cffd5b65db2635
Serkan Sipahi <serkan.sipahi@yahoo.de>
2017-02-02 22:02:08 +0100

    add to ch1/echo1.1.go one more example

--- ch1/echo.1.1.go
+++ ch1/echo.1.1.go
@@ -4,8 +4,22 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"strconv"
 )
 
 func main(){
+
+	// Variant 1 ( can consume n arguments )
 	fmt.Println(strings.Join(os.Args[0:], " "))
+
+	// Variant 2 ( can consue 2 args )
+	val := os.Args[0];
+	// convert val to boolean
+	_, err := strconv.ParseBool(val)
+	// conditions can only work with boolean
+	if err == nil {
+		fmt.Println(os.Args[0] + " " + os.Args[1])
+	}
+
+
 }
