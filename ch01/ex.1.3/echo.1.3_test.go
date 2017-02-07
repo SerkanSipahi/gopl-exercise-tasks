3812943585613de4e980970d6c2d87788f2d9137
Serkan Sipahi <serkan.sipahi@yahoo.de>
2017-02-07 22:40:38 +0100

    add benchmark test with -bench for echo.1.3.go

--- ch01/ex.1.3/echo.1.3_test.go
+++ ch01/ex.1.3/echo.1.3_test.go
@@ -11,3 +11,15 @@ func BenchmarkEcho1(b *testing.B) {
 		Echo1(args)
 	}
 }
+
+func BenchmarkEcho2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Echo2(args)
+	}
+}
+
+func BenchmarkEcho3(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Echo3(args)
+	}
+}
