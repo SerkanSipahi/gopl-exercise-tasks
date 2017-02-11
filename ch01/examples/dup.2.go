7f65eb7d6352aaec7aa654eecda49a76e130c191
Serkan Sipahi <serkan.sipahi@yahoo.de>
2017-02-11 13:44:26 +0100

    add missing function

--- ch01/examples/dup.2.go
+++ ch01/examples/dup.2.go
@@ -3,7 +3,15 @@ package main
 import (
 	"os"
 	"fmt"
+	"bufio"
 )
+
+func countLines(f *os.File, counts map[string]int){
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+}
 
 func main(){
 
